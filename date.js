6d82ba3f31d58f6e44ee544bf8bb0fbe34921ea4
benjamen <teambuddey@gmail.com>
2021-08-27 21:44:07 +0800

    version 1.1 modified date.js

--- date.js
+++ date.js
@@ -19,33 +19,3 @@ exports.getDay = function(){
    return  today.toLocaleDateString("en-US", options)
 
 }
-
-//
-// app.js 
-// const date = require(__dirname + "/date.js")  <now that we have successfully exports 2 functions under 
-//
-// calling function at app.js
-// let day = date.getDate()
-// depending on what options you like to use
-
-
-// module.exports = getDate;
-// module
-// function getDate(){
-//     let today = new Date()
-//     let options = {
-//         weekday: 'long', 
-//         month: 'long',s
-//         day: 'numeric' 
-//     }
-//     let day = today.toLocaleDateString("en-US", options)
-//
-//     return day
-// }
-//
-// app.js
-// const date = require(__dirname + "/date.js")
-//
-// calling function at app.js
-// let day = date()
-//
